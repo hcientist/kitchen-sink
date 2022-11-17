73d50ff6b07936646a343f732925422a13c79423
Michael Stewart <stewarmc@jmu.edu>
2022-11-17 14:55:48 -0500

    in class updates

--- script.js
+++ script.js
@@ -31,5 +31,50 @@ console.log(`i: ${i} relevantDOMElements[i]: ${relevantDOMElements[i].innerText}
 
 
 // 4. when aside clicked, window.prompt then add id with content from prompt to aside's parent, log target
-// 5. add an input with classname as the first child of preceding id's first aside
-// 6. log cowsay for the input
+
+//  using selector pattern A B
+const aside = document.querySelector("body aside");
+aside.addEventListener("click", (ev) => {
+  const userEnteredId = window.prompt("Enter an id");
+  console.log(`userEnteredId: ${userEnteredId}`);
+  // need aside's parent
+  const parent = aside.parentElement;
+  // need to know what the user entered
+  parent.id = userEnteredId;
+  console.log("ev:", ev);
+  console.log(`ev.target: ${ev.target}`);
+
+  // 5. add an input (element of type text) with classname (of your choice) as the first child of preceding id's first aside
+
+  // intended result:
+  // aside
+  //  input of type text with class of your choice
+  //  ol
+
+  // "#id  A"
+
+  const pattern5Result = document.querySelector(`#${userEnteredId} aside`);
+
+  const newInputElem = document.createElement("input");
+  newInputElem.type = "text";
+  newInputElem.className = "best-class-og-343f22-🌈";
+
+  pattern5Result.prepend(newInputElem);
+
+  // 6. log cowsay for the input on blur  // https://helloacm.com/api/cowsay/?msg=CowSay
+
+  // want a ref to the input
+  const inputElemByClassname = document.querySelector(".best-class-og-343f22-🌈");
+  // add event listener to input for blur
+  inputElemByClassname.addEventListener("blur", (ev) => {
+    console.log('event happened')
+  });
+  //    want to get the content of the input
+  //    want to make a request to cowsay api
+  //    when the response is ready, 
+  //      we want to wait around for its text() to be ready,
+  //      then log it
+})
+
+
+
